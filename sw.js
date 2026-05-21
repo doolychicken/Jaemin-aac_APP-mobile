87e92227be22d3333294e49c08d4d54d9438dce0
doolychicken <doolychicken@naver.com>
2026-05-21 12:48:05 +0900

    style: align menu tile sizing

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * On first visit, pre-caches all images so subsequent loads are instant.
  */
 
-const CACHE_VERSION = 'v89';
+const CACHE_VERSION = 'v90';
 const CACHE_NAME = `jaemin-aac-${CACHE_VERSION}`;
 
 self.addEventListener('message', (event) => {
